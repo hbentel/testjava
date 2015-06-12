b169502a7a8fb693a4eea5c43e0035d462c74459
Henrik Bentel <hbentel@gmail.com>
2015-06-12 08:41:00 -0700

    adding little test

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -4,5 +4,17 @@ public class HelloWorld {
   public static void main(String [] args) {
     Greeter greeter = new Greeter();
     System.out.println(greeter.sayHello());
+
+    Integer a = Integer.valueOf(args[0]);
+    Integer b = Integer.valueOf(args[1]);
+    System.out.println("Euclid's answer to GCD: " + Math.abs(gcd(a, b)));
+  }
+
+  private static Integer gcd(int a, int b) {
+    if (b == 0) {
+      return a;
+    }
+    int remainder = a % b;
+    return gcd(b, remainder);
   }
 }
